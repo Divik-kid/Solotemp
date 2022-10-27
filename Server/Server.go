28b2a0d45192b94e0af538ba0e0e8f0f32eb04e5
Chris <christopher.cortsen@gmail.com>
2022-10-27 17:50:21 +0200

    k

--- Server/Server.go
+++ Server/Server.go
@@ -1,10 +1,38 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
+
+	gRPC "github.com/Divik-kid/Solotemp/tree/mainproto"
+
+	"google.golang.org/grpc"
+)
 type Server struct {
 	// an interface that the server needs to have
 	gRPC.UnimplementedTemplateServer
 
 	// here you can impliment other fields that you want
+}
+
+func main() {
+	//start listening on port 6000
+	lis, err := net.Listen("tcp", ":6000")
+	if (error != nil){
+		log.Fatalf("Failed to listen on port 6000: %v", error)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if (error != nil){
+		log.Fatalf("Failed to listen on port 6000: %v", error)
+	}
 }
 
 func (s *Server) SendPost(ctx context.Context, Message *message) (*Message, error) {
